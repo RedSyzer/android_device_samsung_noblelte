2e02788402c864b3cb497dffeae6076a8681658f
LMAO-armv8 <82850873+LMAO-armv8@users.noreply.github.com>
2021-07-02 10:12:52 +0530

    Update and rename revengeos_noblelte.mk to ssos_noblelte.mk

--- ssos_noblelte.mk
+++ ssos_noblelte.mk
@@ -30,10 +30,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/revengeos/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := revengeos_noblelte
+PRODUCT_NAME := ssos_noblelte
 PRODUCT_DEVICE := noblelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
