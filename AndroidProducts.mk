76b8ff96bc9e8ab6ae639dc02009cef36d495fd1
LMAO-armv8 <82850873+LMAO-armv8@users.noreply.github.com>
2021-07-02 10:14:29 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_noblelte.mk
+    $(LOCAL_DIR)/ssos_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_noblelte-user \
-    revengeos_noblelte-userdebug \
-    revengeos_noblelte-eng
+    ssos_noblelte-user \
+    ssos_noblelte-userdebug \
+    ssos_noblelte-eng
