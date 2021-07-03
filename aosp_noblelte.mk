44f73e5acce4d645b8f2abf6b7ba5f08497dd2f8
LMAO-armv8 <82850873+LMAO-armv8@users.noreply.github.com>
2021-07-03 22:13:37 +0530

    Update aosp_noblelte.mk

--- aosp_noblelte.mk
+++ aosp_noblelte.mk
@@ -30,7 +30,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := aosp_noblelte
