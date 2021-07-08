0089f6cdb6f5102b2ba473cf029df2f458cb0e8a
RedSyzer <74223339+RedSyzer@users.noreply.github.com>
2021-07-08 19:15:37 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/corvus_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_noblelte-user \
-    corvus_noblelte-userdebug \
-    corvus_noblelte-eng
+    du_noblelte-user \
+    du_noblelte-userdebug \
+    du_noblelte-eng
