38e2cb05d09884460f44567d3e6a25cdfe1f3d4b
RedSyzer <74223339+RedSyzer@users.noreply.github.com>
2021-07-08 21:53:54 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_noblelte.mk
+    $(LOCAL_DIR)/reloaded_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_noblelte-user \
-    dot_noblelte-userdebug \
-    dot_noblelte-eng
+    reloaded_noblelte-user \
+    reloaded_noblelte-userdebug \
+    reloaded_noblelte-eng
