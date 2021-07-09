a1d4afd15cc830de453a1c0484d02e00fecb7825
RedSyzer <74223339+RedSyzer@users.noreply.github.com>
2021-07-09 23:26:51 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/legion_noblelte.mk
+    $(LOCAL_DIR)/superior_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    legion_noblelte-user \
-    legion_noblelte-userdebug \
-    legion_noblelte-eng
+    superior_noblelte-user \
+    superior_noblelte-userdebug \
+    superior_noblelte-eng
