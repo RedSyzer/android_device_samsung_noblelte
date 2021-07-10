50b656bd78a924e638356948a8fead68ad6badbd
RedSyzer <74223339+RedSyzer@users.noreply.github.com>
2021-07-10 14:31:42 +0530

    Update and rename syberia_noblelte.mk to colt_noblelte.mk

--- colt_noblelte.mk
+++ colt_noblelte.mk
@@ -29,11 +29,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
-# Inherit some common LineageOS stuff.
-#(call inherit-product, vendor/corvus/config/common.mk)
+# Inherit some common Colt stuff.
+  $(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := syberia_noblelte
+PRODUCT_NAME := colt_noblelte
 PRODUCT_DEVICE := noblelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
