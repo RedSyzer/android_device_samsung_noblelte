12b1fce2050ff562ca26722377a34069c87df906
RedSyzer <74223339+RedSyzer@users.noreply.github.com>
2021-07-10 14:32:24 +0530

    Update colt_noblelte.mk

--- colt_noblelte.mk
+++ colt_noblelte.mk
@@ -30,7 +30,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
 # Inherit some common Colt stuff.
-  $(call inherit-product, vendor/colt/config/common_full_phone.mk)
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := colt_noblelte
