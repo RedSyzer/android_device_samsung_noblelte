31c57f7b4ec8fde907067a35da3cd2dcf18695b5
RedSyzer <74223339+RedSyzer@users.noreply.github.com>
2021-07-10 07:06:41 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_noblelte.mk
+    $(LOCAL_DIR)/nad_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_noblelte-user \
-    superior_noblelte-userdebug \
-    superior_noblelte-eng
+    nad_noblelte-user \
+    nad_noblelte-userdebug \
+    nad_noblelte-eng
