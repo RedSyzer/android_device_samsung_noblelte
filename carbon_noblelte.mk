cac5c550440b2e8a3f9db887ea36a54c904815c8
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-13 19:58:52 +0530

    Update carbon_noblelte.mk

--- carbon_noblelte.mk
+++ carbon_noblelte.mk
@@ -29,8 +29,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
-# Inherit some common LineageOS stuff.
-#(call inherit-product, vendor/corvus/config/common.mk)
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := carbon_noblelte
