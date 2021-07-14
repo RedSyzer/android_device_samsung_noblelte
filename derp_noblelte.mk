fb21ead6ba814936ed9fea7822a81514606af360
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-14 17:51:38 +0530

    Update derp_noblelte.mk

--- derp_noblelte.mk
+++ derp_noblelte.mk
@@ -29,8 +29,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
-# Inherit some common DerpFest Rom stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := derp_noblelte
