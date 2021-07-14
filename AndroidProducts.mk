3bce932174fc76cd7698aeef20004e1c6051edc2
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-14 15:47:58 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_noblelte.mk
+    $(LOCAL_DIR)/wave_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_noblelte-user \
-    carbon_noblelte-userdebug \
-    carbon_noblelte-eng
+    wave_noblelte-user \
+    wave_noblelte-userdebug \
+    wave_noblelte-eng
