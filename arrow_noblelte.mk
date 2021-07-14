0ad3797c4d413445f44c99d327f0d4377a62e11b
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-14 07:41:57 +0530

    Update arrow_noblelte.mk

--- arrow_noblelte.mk
+++ arrow_noblelte.mk
@@ -29,7 +29,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
-# Inherit some common ArrowOS stuff
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
