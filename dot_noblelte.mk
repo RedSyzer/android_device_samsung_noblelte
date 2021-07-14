59582dc291489c03e60808045517a88f92d2c835
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-14 12:23:01 +0530

    Update dot_noblelte.mk

--- dot_noblelte.mk
+++ dot_noblelte.mk
@@ -29,8 +29,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
-# Inherit some common LineageOS stuff.
-#(call inherit-product, vendor/corvus/config/common.mk)
+# Inherit some common dotOS stuff
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := dot_noblelte
