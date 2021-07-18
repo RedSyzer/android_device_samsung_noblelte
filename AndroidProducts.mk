42994fd3195374ac88873e4e75971096ea439ffd
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-18 13:43:39 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_noblelte.mk
+    $(LOCAL_DIR)/conquer_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_noblelte-user \
-    carbon_noblelte-userdebug \
-    carbon_noblelte-eng
+    conquer_noblelte-user \
+    conquer_noblelte-userdebug \
+    conquer_noblelte-eng
