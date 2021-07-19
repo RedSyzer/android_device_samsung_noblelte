8bb98b4a7cb8d1985a2ca32bc0c2c3d73604f61c
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-19 20:46:07 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_noblelte.mk
+    $(LOCAL_DIR)/nezuko_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_noblelte-user \
-    dot_noblelte-userdebug \
-    dot_noblelte-eng
+    nezuko_noblelte-user \
+    nezuko_noblelte-userdebug \
+    nazuko_noblelte-eng
