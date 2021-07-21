81572198c1995f92e295735139f1495612cb7edc
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-21 07:17:19 +0530

    Update and rename ancient_noblelte.mk to viper_noblelte.mk

--- viper_noblelte.mk
+++ viper_noblelte.mk
@@ -29,11 +29,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
-# Inherit some common Ancient stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+# Inherit some common ViperOS stuff.
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := ancient_noblelte
+PRODUCT_NAME := viper_noblelte
 PRODUCT_DEVICE := noblelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
