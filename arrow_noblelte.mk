177852bc25ed86562776962abad9fa272a1f8de7
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-21 12:38:38 +0530

    Update arrow_noblelte.mk

--- arrow_noblelte.mk
+++ arrow_noblelte.mk
@@ -29,6 +29,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
+# Inherit from custom vendor
+$(call inherit-product-if-exist, vendor/MiuiCamera/config.mk)
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/arrow/config/common.mk)
 
