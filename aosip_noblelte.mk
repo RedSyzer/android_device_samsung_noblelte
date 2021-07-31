5321407a076704d90b5691879d26d8154440b7d7
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-07-31 10:14:41 +0530

    Update and rename derp_noblelte.mk to aosip_noblelte.mk

--- aosip_noblelte.mk
+++ aosip_noblelte.mk
@@ -29,14 +29,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
-# Inherit some common ROM stuff.
+# Inherit some common AOSiP stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit some VendorExtra stuff
 $(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := derp_noblelte
+PRODUCT_NAME := aosip_noblelte
 PRODUCT_DEVICE := noblelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
