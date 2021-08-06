1147980672dff8ad32ed22bf59471562efe9e3fa
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-08-06 08:43:27 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_noblelte.mk
+    $(LOCAL_DIR)/exui_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_noblelte-user \
-    aosp_noblelte-userdebug \
-    aosp_noblelte-eng
+    exui_noblelte-user \
+    exui_noblelte-userdebug \
+    exui_noblelte-eng
