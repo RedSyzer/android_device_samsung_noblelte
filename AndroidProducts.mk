d6c0c4d05a3aaa7a74ed9c67c6f0236ed6cb8e07
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-08-15 11:39:27 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_noblelte.mk
+    $(LOCAL_DIR)/exthm_noblelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_noblelte-user \
-    lineage_noblelte-userdebug \
-    lineage_noblelte-eng
+    exthm_noblelte-user \
+    exthm_noblelte-userdebug \
+    exthm_noblelte-eng
