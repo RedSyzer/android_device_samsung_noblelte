4456a35535a24cc062b338a8844861d0c7b79b5b
ShuKrenai <74223339+ShuKrenai@users.noreply.github.com>
2021-08-16 14:49:43 +0530

    Update and rename liquid_noblelte.mk to scorpion_noblelte.mk

--- scorpion_noblelte.mk
+++ scorpion_noblelte.mk
@@ -29,22 +29,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from noblelte device
 $(call inherit-product, device/samsung/noblelte/device.mk)
 
-# Inherit some common Liquid stuff.
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+# Inherit some common Scorpion stuff.
+$(call inherit-product, include vendor/scorpion/config/common_full_phone.mk
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := liquid_noblelte
+PRODUCT_NAME := scorpion_noblelte
 PRODUCT_DEVICE := noblelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := SM-N920C
 TARGET_BOOT_ANIMATION_RES = 1440
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="ShuKurenai"
-
-# ONLY FOR OFFICIAL LIQUID TAG
-LIQUID_BUILDTYPE := RELEASE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=nobleltejv \
